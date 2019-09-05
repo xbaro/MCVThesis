a86e076fe8e87918fde8859c334df9708de98e2c
Xavier Baró <xbaro@uoc.edu>
2019-09-05 04:26:13 +0200

    Added status endpoint

---
+++ routes/status.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+var pm2 = require('pm2');
+
+router.get('/', function(req, res, next) {
+    pm2.connect(function(err) {
+        if (err) {
+            res.render('status', {title: 'MCV Theses management system', connected: false, message: err});
+            return;
+        }
+
+        pm2.list(function (err, processDescriptionList) {
+            if (err) {
+                pm2.disconnect();
+                res.render('status', {title: 'MCV Theses management system', connected: false, message: err});
+                return;
+            }
+            var info = [];
+            processDescriptionList.forEach(function(proc) {
+                info.push({name: proc.name, pid: proc.pid, pm_id: proc.pm_id, cpu: proc.monit.cpu, memory: proc.monit.memory, status: proc.pm2_env.status, exec_mode: proc.pm2_env.exec_mode, instances: proc.pm2_env.instances, created_at:proc.pm2_env.created_at});
+            });
+
+            res.render('status', {title: 'MCV Theses management system', connected: true, data: info});
+        });
+    });
+});
+
+module.exports = router;
